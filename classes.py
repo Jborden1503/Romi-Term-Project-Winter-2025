e02f9ae81795b2319e3e7d886c488ef00f83533c
Jborden1503 <jake.borden11@gmail.com>
2025-03-16 13:07:45 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -6,7 +6,11 @@ class Encoder:
     """
     Encoder driver that updates position on demand.
     """
+
     def __init__(self, ch_a_pin, ch_b_pin, enc_timer):
+        '''@:param ch_a_pin: Type, String. Pin for encoder channel A
+        @:param ch_b_pin: Type, String. Pin for encoder channel B
+        @:param enc_timer: Type, int. Timer number for encoder '''
         self.timer = Timer(enc_timer, prescaler=0, period=0xFFFF)
         self.ch_a = self.timer.channel(1, pin=ch_a_pin, mode=Timer.ENC_AB)
         self.ch_b = self.timer.channel(2, pin=ch_b_pin, mode=Timer.ENC_AB)
@@ -16,6 +20,7 @@ class Encoder:
         self.delta = 0
 
     def update(self):
+        '''Updates encoder position. Does not return position'''
         self.last_pos = self.current_pos
         self.current_pos = self.timer.counter()
         self.delta = self.current_pos - self.last_pos
@@ -26,21 +31,31 @@ class Encoder:
         self.position += self.delta
 
     def get_position(self):
+        ''' Returns encoder position. Must call update method first.'''
         return self.position
 
     def zero(self):
+        '''Sets encoder position and time step to zero.'''
         self.position = 0
         self.current_pos = 0
         self.delta = 0
 
+
 class Motordriver:
     """A motor driver for Romi."""
+
     def __init__(self, pwm_pin, dir_pin, enable_pin, timer, channel):
+        '''@:param pwm_pin. Type: str. Timer channel pin to be used for PWM output
+         @:param dir_pin: Type: str. Pin for motor direction pin
+         @:param enable_pin: Type: str. Pin motor enable pin
+         @:param timer: Type: int. Timer number for motor PWM
+         @:param channel: Type: int. Timer channel for motor PWM'''
         self.dir_pin = Pin(dir_pin, Pin.OUT_PP)
         self.enable_pin = Pin(enable_pin, Pin.OUT_PP)
         self.PWM = timer.channel(channel, mode=Timer.PWM, pin=pwm_pin)
 
     def set_effort(self, effort):
+        '''Sets motor effort in %duty cycle. Saturetion function built into this method. Returns nothing'''
         effort = max(min(effort, 100), -100)
         if effort >= 0:
             self.dir_pin.low()  # forward
@@ -50,17 +65,28 @@ class Motordriver:
             self.PWM.pulse_width_percent(-effort)
 
     def enable(self):
+        '''Enables Motor'''
         self.enable_pin.high()
 
     def disable(self):
+        '''Disables motor'''
         self.enable_pin.low()
+
 
 class IRsensor:
     """
-    Driver for an array of line sensors.
+    Driver for an array of 7 analog reflectance sensors.
     """
 
     def __init__(self, PIN_1, PIN_2, PIN_3, PIN_4, PIN_5, PIN_6, PIN_7):
+        '''@:param PIN_1: Type: str. Analog input pin to be used for reading IR sensor values. Sensor #1
+        @:param PIN_2: Type: str. Analog input pin to be used for reading IR sensor values. Sensor #2
+        @:param PIN_3: Type: str. Analog input pin to be used for reading IR sensor values. Sensor #3
+        @:param PIN_4: Type: str. Analog input pin to be used for reading IR sensor values. Sensor #4
+        @:param PIN_5: Type: str. Analog input pin to be used for reading IR sensor values. Sensor #5
+        @:param PIN_6: Type: str. Analog input pin to be used for reading IR sensor values. Sensor #6
+        @:param PIN_7: Type: str. Analog input pin to be used for reading IR sensor values. Sensor #7
+        Pins will be configured as ADCs to read analog IR sensor values. '''
         self.pin_1 = Pin(PIN_1, mode=Pin.IN)
         self.pin_2 = Pin(PIN_2, mode=Pin.IN)
         self.pin_3 = Pin(PIN_3, mode=Pin.IN)
@@ -79,6 +105,8 @@ class IRsensor:
         self.centroid = 0
 
     def set_black(self, black_data=None):
+        '''This method establishes a datum for 'black' reflectance for each sensor in the array individually. Returns
+        a list of integers.'''
         if black_data is None:
             input("Place sensor on BLACK surface and press Enter:")
             for i in range(len(self.adc_list)):
@@ -90,6 +118,8 @@ class IRsensor:
         return self.black_vals
 
     def set_white(self, white_data=None):
+        '''This method establishes a datum for 'white' reflectance for each sensor in the array individually. Returns
+        a list of integers.'''
         if white_data is None:
             input("Place sensor on WHITE surface and press Enter:")
             for i in range(len(self.adc_list)):
@@ -101,6 +131,9 @@ class IRsensor:
         return self.white_vals
 
     def read_line(self):
+        '''This method reads all ADC channels in array and compiles their values into a list. The method also normalizes
+        the values to black or white using linear interpolation. Must be called before get_centroid method
+         Returns the list of white/black values.'''
         for i in range(len(self.adc_list)):
             self.line_vals[i] = self.adc_list[i].read()
             diff = self.black_vals[i] - self.white_vals[i]
@@ -108,6 +141,8 @@ class IRsensor:
         return self.line_pos_list
 
     def get_centroid(self):
+        '''This method takes the list of line values from the read_line method and computes the centroid. Must call
+         read_line method first.'''
         total = sum(self.line_pos_list)
         if total != 0:
             self.centroid = sum(val * (i + 1) for i, val in enumerate(self.line_pos_list)) / total
@@ -116,6 +151,8 @@ class IRsensor:
         return self.centroid
 
     def store_cal(self):
+        '''Routine for calibrating black and white values from .csv file. Or if no file exists, calibrating and storing
+        calibration values in a .csv file.'''
         filelist = listdir()
 
         if "IR_cal.csv" in filelist:
@@ -146,8 +183,14 @@ class IRsensor:
                 for i in range(len(black_data)):
                     file.write("{},{}\n".format(black_data[i], white_data[i]))
 
+
 class PID:
+    ''' Simple PID controller class for constant setpoint. '''
     def __init__(self, Kp, Ki, Kd, setpoint=float(0)):
+        '''@:param Kp: Type: float. Proportional gain.
+        @:param Ki: Type: float. Integral gain.
+        @:param Kd: Type: float. Derivative gain.
+        @:param setpoint: Type: float. PID reference value.'''
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
@@ -157,6 +200,9 @@ class PID:
         self.prev_time = ticks_us()
 
     def update(self, measured_value):
+        '''This method performs PID control
+        @:param measured_value: Feedback value
+        @:returns actuation signal'''
         current_time = ticks_us()
         dt = ticks_diff(current_time, self.prev_time) / 1_000_000  # seconds
         error = self.setpoint - measured_value
@@ -172,6 +218,7 @@ class PID:
         return output
 
 class Gain_compensator:
+    ''' For adjusting actuation values based on battery voltage. '''
     def __init__(self,pin):
         '''@:param pin: Board pin to be configured as an ADC'''
         self.pin = Pin(pin, mode= Pin.IN)
@@ -194,7 +241,8 @@ class IMU: #0x28 is periphiral addr
     ''' This class defines methods and attributes for communicating with a Bosch BNO55 IMU. The communication protocol
      will be I2C using the micropython library, pyb.I2C. This class accepts a controller object created externally.  '''
     def __init__(self, controller):
-        ''' Controller object should be a pyb.I2C controller object'''
+        ''' Controller object should be a pyb.I2C controller object
+        @:param controller: Type: I2C controller object.'''
         self.controller = controller
         self.acc_status = 0
         self.sys_status = 0
@@ -282,7 +330,7 @@ class Bumpsensor:
     def __init__(self,sensorPins):
         self.bumpPins = []
         for pin in sensorPins:
-            self.bumpPins.append(Pin(pin, mode=Pin.IN, pull=Pin.PULL_UP))
+            self.bumpPins.append(pyb.Pin(pin, mode=pyb.Pin.IN, pull=pyb.Pin.PULL_UP))
     def get_state(self):
         """Returns True if any bump sensor is triggered , otherwise False."""
         for pin in self.bumpPins:
